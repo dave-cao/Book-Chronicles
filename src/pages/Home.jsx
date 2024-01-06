aa4c3d8076f35efd3088c23315bac6c30ced29c3
David Cao <sirdavidcao@gmail.com>
2024-01-05 22:36:28 -0700

    implemented pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,23 +15,36 @@ function Home({ supabase, session }) {
   const [posts, setPosts] = useState([])
   const [sorts, setSorts] = useState({ created_at: false, vote: true })
   const [search, setSearch] = useState("")
+  const [totalPosts, setTotalPosts] = useState(0)
+  const [page, setPage] = useState(1)
   const user = session ? session.user : { user_metadata: {} }
   const user_id = user.id
+  const POST_OFFSET = 6
 
-  const getPosts = async () => {
-    const { data, error } = await supabase.from("posts").select().order("created_at", { ascending: false });
+  const getPosts = async (pageNumber) => {
+    const startRange = (pageNumber - 1) * POST_OFFSET
+    const endRange = startRange + POST_OFFSET - 1
+    const { data, error } = await supabase.from("posts").select().range(startRange, endRange).order("created_at", { ascending: false });
     setPosts(data);
+  }
+
+  const getTotalPosts = async () => {
+    const { data, error } = await supabase.from("posts").select('*', { count: 'exact' })
+    setTotalPosts(data.length)
   }
 
 
   // get books when user goes to home page
   useEffect(() => {
-    const gettingPosts = getPosts();
+    const gettingPosts = getPosts(1);
     toast.promise(gettingPosts, {
       loading: 'Loading your posts...',
       success: 'Bookshelf Loaded',
       error: 'Error when fetching',
     })
+
+    // get total count of posts
+    getTotalPosts()
   }, [])
 
   // update the search bar state values
@@ -91,6 +104,39 @@ function Home({ supabase, session }) {
     setPosts(postsCopy)
   }
 
+  // display pagination using bootstrap
+  const pagination = () => {
+    const paginationLength = Math.ceil(totalPosts / POST_OFFSET)
+    const arr = Array.from({ length: paginationLength })
+    return arr.map((_, index) => {
+      const currentPage = index + 1
+      const component = currentPage === page ? <li className="page-item active"><a className="page-link">{currentPage}</a></li> : <li onClick={() => { handlePaginationClick(currentPage) }} className="page-item"><a href="#" className="page-link">{currentPage}</a></li>
+      return component
+    })
+  }
+
+  // pagination functions
+  const previousPage = () => {
+    if (page > 1) {
+      setPage((page) => page - 1)
+      getPosts(page - 1)
+
+    }
+  }
+
+  const nextPage = () => {
+    if (page < Math.ceil(totalPosts / POST_OFFSET)) {
+      setPage((page) => page + 1)
+      getPosts(page + 1)
+    }
+  }
+
+  const handlePaginationClick = (pageNumber) => {
+    setPage(pageNumber)
+    getPosts(pageNumber)
+  }
+
+
 
 
   return (
@@ -125,6 +171,27 @@ function Home({ supabase, session }) {
       {/* the post previews */}
       <div className="post-preview-container">
         {displayPostPreviews()}
+      </div>
+
+      {/* pagination */}
+      <div>
+        <nav aria-label="Page navigation" className="page-navigation">
+          <ul className="pagination">
+            <li onClick={previousPage} class="page-item">
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+              </a>
+            </li>
+            {pagination()}
+            <li onClick={nextPage} class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
     </>
